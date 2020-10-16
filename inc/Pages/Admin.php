0bfe22b2de1512e9443ad4f9c7f0f625f79fe50d
Arian <ariansani.18@ichat.sp.edu,sg>
2020-10-16 11:29:27 +0800

    Lesson 16: dealing with callbacks

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,30 +6,47 @@ namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
+use \Inc\Api\Callbacks\AdminCallbacks;
 
   class Admin extends BaseController{
 
     public $settings;
+
+    public $callbacks;
     
     public $pages = array();
 
     public $subpages = array();
 
-    public function __construct(){
+
+    public function register(){
+      //add_action('admin_menu', array($this,'add_admin_pages'));
       $this->settings = new SettingsApi();
 
+      $this->callbacks = new AdminCallbacks();
+
+      $this->setPages();
+
+      $this->setSubPages();
+
+      $this->settings->addPages($this->pages)->withSubPage( 'Dashboard' )->addSubPages($this->subpages)->register();
+    }
+
+    public function setPages(){
       $this->pages = array(
         array(
         'page_title'=> 'Arian Sani Plugin',
         'menu_title'=> 'ArianSani',
         'capability'=> 'manage_options', 
         'menu_slug'=> 'ariansani_plugin',
-        'callback'=> function(){echo '<h1>Arian Sani Plugin</h1>';},
+        'callback'=> array($this->callbacks,'adminDashboard'),
         'icon_url'=> 'dashicons-store', 
         'position'=> 110 
         )
       );
+    }
 
+    public function setSubpages(){
       $this->subpages =  array(
         array(
             'parent_slug'=> 'ariansani_plugin',
@@ -55,14 +72,6 @@ use \Inc\Api\SettingsApi;
           'callback'=> function(){echo '<h1>Widgets Manager</h1>';}
       )
       );
-
-
-    }
-
-    public function register(){
-      //add_action('admin_menu', array($this,'add_admin_pages'));
-     
-      $this->settings->addPages($this->pages)->withSubPage( 'Dashboard' )->addSubPages($this->subpages)->register();
     }
 
   }
