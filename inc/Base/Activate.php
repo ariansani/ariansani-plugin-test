f5e7c8a7c1af9a0e7d126ddab9f3001478964831
Arian <ariansani.18@ichat.sp.edu,sg>
2020-10-20 10:31:53 +0800

    lesson 23: save default options on activate

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -7,5 +7,14 @@ namespace Inc\Base;
  class Activate{
      public static function activate(){
          flush_rewrite_rules();
+
+         if(get_option('ariansani_plugin')){
+             return;
+         } //if it is set, return and stop running the script. 
+
+         $default = array();
+         update_option('ariansani_plugin',$default);
+
+
      }
  }
