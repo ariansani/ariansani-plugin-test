c1c9a26dd7bb6cb976396e49253f520c1c491e53
Arian <ariansani.18@ichat.sp.edu,sg>
2020-10-21 11:27:48 +0800

    Lesson27: store arrays in wp_options

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -4,8 +4,6 @@
  */
 namespace Inc\Api\Callbacks;
 
-use Inc\Base\BaseController;
-
 class CptCallbacks{
 
 
@@ -13,31 +11,37 @@ class CptCallbacks{
         echo 'Create as many Custom Post Types as you want.';
     }
 
-    public function cptSanitize($input ){
-        return $input;
-    }
+	public function cptSanitize( $input )
+	{
+		$output = get_option('alecaddd_plugin_cpt');
 
-    public function textField($args){
-        $name= $args['label_for'];
-        $option_name= $args['option_name'];
-        $input = get_option($option_name);
+		foreach ($output as $key => $value) {
+			if ($input['post_type'] === $key) {
+				$output[$key] = $input;
+			} else {
+				$output[$input['post_type']] = $input;
+			}
+		}
+		
+		return $output;
+	}
 
-        $value = $input[$name];
+    public function textField( $args )
+	{
+		$name = $args['label_for'];
+		$option_name = $args['option_name'];
+		$input = get_option( $option_name );
 
-        echo '<input type="text" class="regular-text" id="'.$name.'" name="'.$option_name.'['. $name . 
-        ']" value="'.$value.'" placeholder="'.$args['placeholder'].'">';
-    }
+		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
+	}
 
+	public function checkboxField( $args )
+	{
+		$name = $args['label_for'];
+		$classes = $args['class'];
+		$option_name = $args['option_name'];
+		$checkbox = get_option( $option_name );
 
-    public function checkboxField($args){
-        $name= $args['label_for'];
-        $classes= $args['class'];
-        $option_name= $args['option_name'];
-        $checkbox = get_option($option_name);
-
-        $checked = isset($checkbox[$name])? ($checkbox[$name]? true : false):false;
-
-        echo '<div class="'.$classes.'"><input type="checkbox" id="'.$name.'" name="'.$option_name.'['. $name . ']"value="1" class="'.
-        $classes .'"'. ( $checked ?'checked':'') .'><label for="'.$name.'"><div></div></label></div>';
-    }
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
+	}
 }
